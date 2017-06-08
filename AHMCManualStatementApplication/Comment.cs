69fb9d29a3717e9154903f4258a3cc2bd7c56e1f
cynthia.quach <cynthia.quach@ahmchealth.com>
2017-06-08 11:52:33 -0700

    Adds Comment class

---
+++ AHMCManualStatementApplication/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AHMCManualStatementApplication
+{
+    public class Comment
+    {
+        [Key]
+        public DateTime DateRequested { get; set; }
+        public int FacilityID { get; set; }
+        public string AcctNumber { get; set; }
+        public DateTime CommentDate { get; set; }
+        public string CommentNote { get; set; }
+    }
+}
