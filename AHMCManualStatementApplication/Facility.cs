44dc72b6b2e415b6b9c0842a128f9e24702d2737
cynthia.quach <cynthia.quach@ahmchealth.com>
2017-06-08 11:43:40 -0700

    Adds Facility class

---
+++ AHMCManualStatementApplication/Facility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace AHMCManualStatementApplication
+{
+    public class Facility
+    {
+        [Key]
+        public int FacilityId { get; set; }
+        public string FacilityAbbr { get; set; }
+        public string FacilityName { get; set; }
+        public string FacilityAddress { get; set; }
+        public string FacilityCity { get; set; }
+        public string FacilityState { get; set; }
+        public string FacilityZip { get; set; }
+        public string FacilityTelephone { get; set; }
+        public string RemitName { get; set; }
+        public string RemitAddress { get; set; }
+        public string RemitCity { get; set; }
+        public string RemitState { get; set; }
+        public string RemitZip { get; set; }
+    }
+}
