05972714e4b38c1da8e69bcb096453635160cdb2
cynthia.quach <cynthia.quach@ahmchealth.com>
2017-06-19 12:02:30 -0700

    Adds SearchByAccount form to allow user to query data by account number;

---
+++ AHMCManualStatementApplication/SearchByAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AHMCManualStatementApplication
+{
+    public partial class SearchByAccount : MetroFramework.Forms.MetroForm
+    {
+        public SearchByAccount()
+        {
+            InitializeComponent();
+        }
+    }
+}
